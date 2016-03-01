15199efc472524f1d5f150af5a9b443bb20d0aab
nik <nik_rb@yahoo.com>
2016-03-01 02:25:47 +0000

    basic json rest-ish service

---
+++ routes/drills.js
@@ -0,0 +1,23 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var mdb;
+var drills;
+
+var url = 'mongodb://localhost:27017/shoot180';
+MongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected.");
+    
+    drills = db.collection( 'drills');
+    // db.close();
+});
+
+exports.findAll = function( req, res){
+    drills.find().toArray( function(err, items){
+        if( err){
+            console.log( "fetch drills failed");
+        } else {
+            res.send( items);
+        }
+    });
+}
